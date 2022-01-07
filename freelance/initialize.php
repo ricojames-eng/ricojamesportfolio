8182f7a481c62ce27918855685249cc08db868de
ricojames-eng <ricojames.quirante@lorma.edu>
2022-01-08 04:00:52 +0800

    Portfolio

---
+++ freelance/initialize.php
@@ -0,0 +1,10 @@
+<?php
+$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
+if(!defined('base_url')) define('base_url','http://localhost/freelance/');
+if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+if(!defined('dev_data')) define('dev_data',$dev_data);
+if(!defined('DB_SERVER')) define('DB_SERVER',"remotemysql.com");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"pNKzi39B6c");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"Pen6jnsdwF");
+if(!defined('DB_NAME')) define('DB_NAME',"pNKzi39B6c");
+?>
